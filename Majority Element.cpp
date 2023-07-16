e055df9013128d9ff983d4b3926ac3a2cdf33cc6
pndeyharshit <138971073+pndeyharshit@users.noreply.github.com>
2023-07-17 00:46:36 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int> v) {
+
+    int n = v.size();
+
+    for (int i = 0; i < n; i++) {
+        //selected element is v[i]
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            // counting the frequency of v[i]
+            if (v[j] == v[i]) {
+                cnt++;
+            }
+        }
+
+        if (cnt > (n / 2))
+            return v[i];
+    }
+
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
+    return 0;
+}
+
